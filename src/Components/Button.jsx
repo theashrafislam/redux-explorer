1a00af492418330b29933850a2e37e0dbb937e43
Ashraful Islam <theashrafislam@gmail.com>
2025-03-09 01:10:45 +0600

    final version

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
-const Button = ({ type, onClick }) => {
+const Button = ({ type }) => {
   return (
     <button
-      onClick={onClick}
       className={`px-4 py-2 text-white uppercase font-semibold rounded-lg shadow-md transition-all duration-300 ${
         type === "increase" ? "bg-green-500 hover:bg-green-600" : "bg-red-500 hover:bg-red-600"
       }`}
